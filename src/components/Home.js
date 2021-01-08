ab047f586e722519fa4ef1b2beb0f9c28e962617
Melindadiaz07 <melindadiaz@me.com>
2021-01-08 17:35:28 -0500

    Set up state to update destination after click event on mapped image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import archway from '../images/temp-archway.png'
 
 
 const Home = () => {
 
+  const [destination, setDestination] = useState("null")
+
+
+  const portalClick = (e) => {
+    e.preventDefault()
+    setDestination(e.target.title)
+  }
+
+  const updateDestDisplay = () => {
+    console.log(destination)
+  }
+
+  useEffect(updateDestDisplay, [destination])
+
+
+
   return(
   <div>
-    <img src={archway} alt="archway" usemap="#image-map"/>
+    <img src={archway} alt="archway" useMap="#image-map"/>
 
 
    
     <map name="image-map">
-        <area target="_self" alt="pyramid" title="pyramid" href="pyramid" coords="239,345,75" shape="circle" />
-        <area target="_self" alt="desert" title="desert" href="desert" coords="557,86,59" shape="circle" />
-        <area target="_self" alt="forest" title="forest" href="forest" coords="868,348,80" shape="circle" />
+        <area  alt="pyramid" title="pyramid" href="pyramid" coords="239,345,75" shape="circle" onClick={(e) => portalClick(e)}/>
+        <area  alt="desert" title="desert" href="desert" coords="557,86,59" shape="circle" onClick={(e) => portalClick(e)}/>
+        <area  alt="forest" title="forest" href="forest" coords="868,348,80" shape="circle" onClick={(e) => portalClick(e)}/>
     </map>
+
+
+    
   </div>
 
   )

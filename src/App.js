66a87235fe26fde3bf79e3c05e0a3bdba6923886
Melindadiaz07 <melindadiaz@me.com>
2021-01-09 17:14:33 -0500

    Set up Nav component with router to redirect to map component when clicked

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Home from './components/Home'
 import Nav from './components/Nav'
 import Map from './components/Map'
@@ -6,9 +12,26 @@ import Map from './components/Map'
 function App() {
   return (
     <div className="App">
-      <Nav />
-      {/* <Home /> */}
-      <Map />
+
+      <Router>
+        <Nav />
+
+      <Switch>
+
+        <Route exact path="/" >
+          <Home />
+        </Route>
+
+        <Route exact path="/Map">
+          <Map />
+        </Route>
+
+      
+      </Switch>
+
+    
+
+      </Router>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import  { Redirect, useHistory, NavLink } from 'react-router-dom'
+import compass from  '../images/compass.png'
 
 const Nav = () => {
+
+
+  // const history = useHistory()
+
+  // const openMap = () => {
+  //   console.log("clicked compass")
+  //   history.push('/map')
+  // }
 
   return(
 
     <div>
-      Nav
-      {/* Clickable icon to navigate back to map - a compass maybe? */}
+     <NavLink to="/map" >
+        <img src={compass} alt="compass icon" id="compass-icon" />
+    </NavLink> 
+      
     </div>
   )
 }

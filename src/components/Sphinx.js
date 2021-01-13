57f6b8261c18b9ef44f08a7d78080f34a6addcad
Melindadiaz07 <melindadiaz@me.com>
2021-01-13 13:34:38 -0500

    Added image and text to sphinx component, set up grid layout

--- src/components/Sphinx.js
+++ src/components/Sphinx.js
@@ -1,11 +1,20 @@
 import React, { useEffect } from 'react';
+import sphinx from '../images/sphinx-front-view.jpg'
 
 const Sphinx = () => {
 
 
   return(
-    <div>
-      Sphinx
+    <div id="sphinx-container">
+      
+      <img src={sphinx} alt="Giza sphinx front view" id="sphinx-image"/>
+
+      <p className="artifact-description" >The Dream Stele, also called the Sphinx Stele, is an epigraphic stele erected 
+      between the front paws of the Great Sphinx of Giza by the ancient Egyptian pharaoh 
+      Thutmose IV in the first year of the king's reign, 1401 BC, during the 18th Dynasty. 
+      As was common with other New Kingdom rulers, the epigraph makes claim to a divine 
+      legitimisation of kingship.
+      </p>
     </div>
 
   )
